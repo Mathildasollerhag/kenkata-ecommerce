588c5ab86cc3bacb587bb3b9baa5d45be06a9220
ElvinSalim1 <elvinsalim5@gmail.com>
2020-09-14 21:14:14 +0200

    new commit navbar

--- src/components/global/Navigation.vue
+++ src/components/global/Navigation.vue
@@ -1,2 +1,27 @@
 <template>
+    <div>
+        <firstNavigation/>
+        <secondNavigation />
+        <hr>
+        <thirdNavigation />
+    </div>
 </template>
+
+<script>
+import firstNavigation from '@/components/global/firstNavigation.vue'
+import secondNavigation from '@/components/global/secondNavigation.vue'
+import thirdNavigation from '@/components/global/thirdNavigation.vue'
+export default {
+    name: 'Navbar',
+    components: {
+        firstNavigation,
+        secondNavigation,
+        thirdNavigation
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>

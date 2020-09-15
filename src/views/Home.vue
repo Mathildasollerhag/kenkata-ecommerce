db837f953c3fe1f72377a60dbdd185bba9821cdd
Christoffer Olsson <55234320+Chrilla@users.noreply.github.com>
2020-09-15 10:12:00 +0200

    Subscribe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="home">
+  <div>
   
+    <SubscribeBanner/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import SubscribeBanner from '@/components/home/SubscribeBanner.vue'
 
 export default {
   name: 'Home',
   components: {
-    
+    SubscribeBanner,
   }
 }
 </script>

378c0485f43a2e261437b8e11d70c59e4aa8be93
Mathilda Sollerhag <mathilda.sollerhag@gmail.com>
2020-09-15 14:28:26 +0200

    Banners commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="home">
-  
+    <InfoCards />
+    <ShopByCategory />
+    <DiscountBanner />
+    <FlashBanner />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import InfoCards from '../components/home/InfoCards';
+import ShopByCategory from '../components/home/ShopByCategory';
+import DiscountBanner from '../components/home/DiscountBanner';
+import FlashBanner from '../components/home/FlashBanner';
 
 export default {
   name: 'Home',
   components: {
-    
+    InfoCards,
+    ShopByCategory,
+    DiscountBanner,
+    FlashBanner
   }
 }
 </script>

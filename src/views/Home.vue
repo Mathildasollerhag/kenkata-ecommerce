86e2f118609d3b23d2f6e7f8eea48a6172682b8b
ElvinSalim1 <elvinsalim5@gmail.com>
2020-09-15 14:22:35 +0200

    new commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
-  
+    <featuredProducts/>
   </div>
 </template>
 
 <script>
+import featuredProducts from '../components/global/featuredProducts'
 // @ is an alias to /src
 
 export default {
   name: 'Home',
   components: {
-    
+    featuredProducts
   }
 }
 </script>

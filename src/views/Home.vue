babf8004693dc3c438519692814dbeb36c725d30
Christoffer Olsson <55234320+Chrilla@users.noreply.github.com>
2020-09-15 10:32:29 +0200

    LatestBlog

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-  
+    
+    <LatestBlog/>
     <SubscribeBanner/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import LatestBlog from '@/components/home/LatestBlog.vue'
 import SubscribeBanner from '@/components/home/SubscribeBanner.vue'
 
 export default {
   name: 'Home',
   components: {
+    LatestBlog,
     SubscribeBanner,
   }
 }

7ab41b7a390cffaf284f27817804bda8a91c2438
Christoffer Olsson <55234320+Chrilla@users.noreply.github.com>
2020-09-16 11:32:12 +0200

    ShopPage

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Shop from '../views/Shop.vue'
 //Import .vue - file here
 
 Vue.use(VueRouter)
@@ -16,71 +17,71 @@ const routes = [
     name: 'Shop',
     component: Shop
   },
-  {
-    path: '/product:id',
-    name: 'ProductDetails',
-    component: ProductDetails
-  },
-  {
-    path: '/quickview???',
-    name: 'QuickView',
-    component: QuickView
-  },
-  {
-    path: '/shoppingcart',
-    name: 'ShoppingCart',
-    component: ShoppingCart
-  },
-  {
-    path: '/checkout',
-    name: 'Checkout',
-    component: Checkout
-  },
-  {
-    path: '/account',
-    name: 'MyAccount',
-    component: MyAccount
-  },
-  {
-    path: '/wishlist',
-    name: 'Wishlist',
-    component: Wishlist
-  },
-  {
-    path: '/compare',
-    name: 'Compare',
-    component: Compare
-  },
-  {
-    path: '/404???',
-    name: '404',
-    component: 404
-  },
-  {
-    path: '/about',
-    name: 'AboutUs',
-    component: AboutUs
-  },
-  {
-    path: '/team',
-    name: 'Team',
-    component: Team
-  },
-  {
-    path: '/portfolio',
-    name: 'Portfolio',
-    component: Portfolio
-  },
-  {
-    path: '/portfolio-details???',
-    name: 'PortfolioDetails',
-    component: PortfolioDetails
-  },
-  {
-    path: '/contact',
-    name: 'ContactUs',
-    component: ContactUs
-  }
+  // {
+  //   path: '/product:id',
+  //   name: 'ProductDetails',
+  //   component: ProductDetails
+  // },
+  // {
+  //   path: '/quickview???',
+  //   name: 'QuickView',
+  //   component: QuickView
+  // },
+  // {
+  //   path: '/shoppingcart',
+  //   name: 'ShoppingCart',
+  //   component: ShoppingCart
+  // },
+  // {
+  //   path: '/checkout',
+  //   name: 'Checkout',
+  //   component: Checkout
+  // },
+  // {
+  //   path: '/account',
+  //   name: 'MyAccount',
+  //   component: MyAccount
+  // },
+  // {
+  //   path: '/wishlist',
+  //   name: 'Wishlist',
+  //   component: Wishlist
+  // },
+  // {
+  //   path: '/compare',
+  //   name: 'Compare',
+  //   component: Compare
+  // },
+  // {
+  //   path: '/404???',
+  //   name: '404',
+  //   component: 404
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'AboutUs',
+  //   component: AboutUs
+  // },
+  // {
+  //   path: '/team',
+  //   name: 'Team',
+  //   component: Team
+  // },
+  // {
+  //   path: '/portfolio',
+  //   name: 'Portfolio',
+  //   component: Portfolio
+  // },
+  // {
+  //   path: '/portfolio-details???',
+  //   name: 'PortfolioDetails',
+  //   component: PortfolioDetails
+  // },
+  // {
+  //   path: '/contact',
+  //   name: 'ContactUs',
+  //   component: ContactUs
+  // }
 ]
 
 const router = new VueRouter({

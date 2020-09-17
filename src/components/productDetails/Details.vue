074e0d702429a8139c6393b827d76a955a4f3018
Viktor <sandvikviktor@live.com>
2020-09-17 11:37:14 +0200

    Gjorde productDetails till en komponent för att återanvända på Quickview

---
+++ src/components/productDetails/Details.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container d-flex align-items-center">
+    <DetailsCarousel :product="product" />
+    <div class="details">
+      <h3 class="theme-text mb-3">{{product.name}}</h3>
+      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud.</p>
+      <hr />
+
+      <!-- Price Row -->
+      <div class="d-flex align-items-center">
+        <h4 class="m-0 theme-text mr-3 my-1">{{product.price}} SEK</h4>
+        <p class="m-0">
+          <del class="text-muted mr-4">$220.00</del>
+        </p>
+        <img class="icon mb-1 mr-1" src="@/images/icons/InStock.png" alt />
+        <p class="m-0">In Stock</p>
+      </div>
+
+      <!-- Options -->
+      <div class="d-flex align-items-center mt-3 mb-4">
+        <Quantity />
+
+        <!-- Add To Cart -->
+        <button class="btn btn-kenkata-blue font-weight-normal mx-4">
+          <img src="@/images/icons/Cart.png" alt /> Add to cart
+        </button>
+
+        <!-- Colors/Compare/Wishlist -->
+        <div class="d-flex options-img">
+          <img src="@/images/icons/ColorCircle.png" />
+          <span class="c-pointer" v-b-tooltip.hover title="Compare">
+            <img class="mx-2" src="@/images/icons/CompareCircle.png" />
+          </span>
+          <span class="c-pointer" v-b-tooltip.hover title="Add To Wishlist">
+            <img src="@/images/icons/HeartCircle.png" />
+          </span>
+        </div>
+      </div>
+
+      <!-- Category -->
+      <p>
+        Category:
+        <span class="theme-text">Shoes</span>
+      </p>
+
+      <!-- Tags -->
+      <p class="tags d-block my-4">
+        Tags:
+        <span class="ml-2">Fashion</span>
+        <span class="mx-1">Shoes</span>
+        <span>Sneakers</span>
+      </p>
+
+      <!-- Share -->
+      <div class="details-socials d-flex">
+        <span class="mr-3">
+          <p>Share:</p>
+        </span>
+        <DetailsSocials />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import DetailsCarousel from "./DetailsCarousel";
+import DetailsSocials from "./DetailsSocials";
+import ProductReviews from "./ProductReviews";
+import Quantity from "./Quantity";
+export default {
+    props: ["product"],
+    components: {
+        DetailsCarousel,
+        DetailsSocials,
+        ProductReviews,
+        Quantity
+    }
+};
+</script>
+
+<style scoped>
+.details {
+  padding-left: 12%;
+}
+.icon {
+  transform: scale(0.9);
+}
+.options-img img {
+  transform: scale(0.9);
+}
+.btn-kenkata-blue {
+  text-transform: unset;
+}
+button img {
+  filter: brightness(0) invert(1);
+}
+button:hover img {
+  filter: unset;
+}
+
+.tags span {
+  border: 1px solid var(--gray-theme);
+  padding: 0 5px;
+}
+</style>
--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -7,67 +7,7 @@
       </div>
 
       <!-- Product Details -->
-      <div class="container d-flex align-items-center">
-        <DetailsCarousel :product="product" />
-        <div class="details">
-          <h3 class="theme-text mb-3">{{product.name}}</h3>
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud.</p>
-          <hr />
-
-          <!-- Price Row -->
-          <div class="d-flex align-items-center">
-            <h4 class="m-0 theme-text mr-3 my-1">{{product.price}} SEK</h4>
-            <p class="m-0">
-              <del class="text-muted mr-4">$220.00</del>
-            </p>
-            <img class="icon mb-1 mr-1" src="@/images/icons/InStock.png" alt="">
-            <p class="m-0">In Stock</p>
-          </div>
-
-          <!-- Options -->
-          <div class="d-flex align-items-center mt-3 mb-4">
-            <Quantity />
-
-            <!-- Add To Cart -->
-            <button class="btn btn-kenkata-blue font-weight-normal mx-4">
-              <img src="@/images/icons/Cart.png" alt=""> Add to cart
-            </button>
-
-            <!-- Colors/Compare/Wishlist -->
-            <div class="d-flex options-img">
-              <img src="@/images/icons/ColorCircle.png">
-              <span class="c-pointer" v-b-tooltip.hover title="Compare">
-              <img class="mx-2" src="@/images/icons/CompareCircle.png">
-              </span>
-              <span class="c-pointer" v-b-tooltip.hover title="Add To Wishlist">
-                <img src="@/images/icons/HeartCircle.png">
-              </span>              
-            </div>
-          </div>
-
-          <!-- Category -->
-          <p>
-            Category:
-            <span class="theme-text">Shoes</span>
-          </p>
-
-          <!-- Tags -->
-          <p class="tags d-block my-4">
-            Tags:
-            <span class="ml-2">Fashion</span>
-            <span class="mx-1">Shoes</span>
-            <span>Sneakers</span>
-          </p>
-
-          <!-- Share -->
-          <div class="details-socials d-flex">
-            <span class="mr-3">
-              <p>Share:</p>
-            </span>
-            <DetailsSocials />
-          </div>
-        </div>
-      </div>
+      <Details :product="product"/>
 
       <!-- Details Nav -->
       <div class="container mb-custom mt-5">
@@ -167,6 +107,7 @@
 <script>
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import ProductTopRow from "@/components/productDetails/ProductTopRow";
+import Details from "@/components/productDetails/Details"
 import DetailsCarousel from "@/components/productDetails/DetailsCarousel";
 import DetailsSocials from "@/components/productDetails/DetailsSocials";
 import ProductReviews from "@/components/productDetails/ProductReviews";
@@ -181,6 +122,7 @@ export default {
   props: ["id"],
   components: {
     ProductTopRow,
+    Details,
     DetailsCarousel,
     DetailsSocials,
     ProductReviews,
@@ -203,28 +145,6 @@ export default {
 </script>
 
 <style scoped>
-.details {
-  padding-left: 12%;
-}
-.icon {
-  transform: scale(0.9);
-}
-.options-img img {
-  transform: scale(0.9);
-}
-.btn-kenkata-blue {
-  text-transform: unset;
-}
-button img {
-  filter: brightness(0) invert(1);
-}
-button:hover img {
-  filter: unset;
-}
-.tags span {
-  border: 1px solid var(--gray-theme);
-  padding: 0 5px;
-}
 
 .details-nav .nav-link {
   margin: 0 2px;
@@ -248,8 +168,6 @@ button:hover img {
   background-color: var(--theme) !important;
   border-color: var(--theme);
 }
-
-
 </style>
 
 <style>

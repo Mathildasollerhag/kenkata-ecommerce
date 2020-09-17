bb8b485f1df1b1d74508677d6acd54a4945d36cc
atwehalaa <atwehalaa@gmail.com>
2020-09-17 07:59:17 +0200

    sista

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,20 +7,20 @@
     <LatestBlog/>
     <SubscribeBanner/>
 
-  <div class="home">
+    <div class="home">
 
     <!-- New Arrivals -->
     <div class="my-5 container">
-        <div class="text-center">
-          <div class="d-flex justify-content-center text-uppercase">
-            <h2 class="text-kenkata-darkblue font-weight-bold">New</h2>
-            <h2 class="text-kenkata-blue font-weight-bold ml-2">Arrivals</h2>
-          </div>
+            <div class="text-center">
+              <div class="d-flex justify-content-center text-uppercase">
+               <h2 class="text-kenkata-darkblue font-weight-bold">New</h2>
+               <h2 class="text-kenkata-blue font-weight-bold ml-2">Arrivals</h2>
+             </div>
           <p class="my-3">Check out our latest products from top fashion designers</p>
         </div>
       <NewArrivals />
     </div>
-
+</div>
 
     <featuredProducts/>
     <InfoCards />
@@ -30,6 +30,7 @@
 
 
 
+  
   </div>
 </template>
 

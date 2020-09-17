d8a1b515ce982697ecd1c91d07b5ee6795fa3c27
Mathilda Sollerhag <mathilda.sollerhag@gmail.com>
2020-09-17 15:43:12 +0200

    Börjat på team-korten

---
+++ src/components/team/TeamCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div>
+        <img src="@/images/Team.png" alt="Image">
+        <div class="theme-bg">
+            <p>John Doe</p>
+            <small>Frontend Developer</small>
+        </div>
+    </div>
+    
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>

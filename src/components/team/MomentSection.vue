56fbb9b719edccb3229464505eec6aceb290c44e
Mathilda Sollerhag <mathilda.sollerhag@gmail.com>
2020-09-22 11:04:28 +0200

    View Teams är klar

---
+++ src/components/team/MomentSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="blue-theme-bg mt-5">
+    <div  class="text-center pt-10">
+        <div class="d-flex justify-content-center">
+            <h2 class="text-white font-weight-bold">Some</h2><h2 class="font-weight-bold theme-text ml-2">special moments</h2>
+        </div>
+        <h2 class="text-white font-weight-bold mb-4">from our company</h2>
+        <p class="text-white pb-4">It has survived not only five centuries, but also the leap into electronic <br> typesetting, remaining essentially unchanged.</p>
+    </div>
+    <div class="d-flex justify-content-between mb-10">
+        <img class="img-fluid" src="@/images/Content.png" alt="Image">
+        <img class="img-fluid" src="@/images/Content.png" alt="Image">
+        <img class="img-fluid" src="@/images/Content.png" alt="Image">
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.pt-10 {
+    padding-top: 6rem;
+}
+
+.mb-10 {
+    margin-bottom: 6rem;
+}
+
+</style>>

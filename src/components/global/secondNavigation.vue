cbbb1ded0cb507d0907331eb01a89b69550c73e0
Christoffer Olsson <55234320+Chrilla@users.noreply.github.com>
2020-10-06 10:25:24 +0200

    text-center secondNavigation

--- src/components/global/secondNavigation.vue
+++ src/components/global/secondNavigation.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="container pt-4 pb-4">
             <div class="row">
-                <div class="col-12 col-xl-2"><router-link class="navbar-brand" id="brand" to="/"><img src="../../images/logo.png" alt=""></router-link></div>
+                <div class="col-12 col-xl-2 text-center"><router-link class="navbar-brand" id="brand" to="/"><img src="../../images/logo.png" alt=""></router-link></div>
                 <div class="col-12 col-xl-6">
                     <div class="input-group class1">
                         <div class="col-5 col-md-6 col-xl-7 pr-0 pl-0"><input id="myclass5" type="text" class="form-control" placeholder="Search products..." aria-label="Search products..." aria-describedby="basic-addon2"></div>
